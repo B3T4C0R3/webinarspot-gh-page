ce158a06e23d7d2020d35164a9371deadde0f20a
B3T4C0R3 <B3T4C0R3@users.noreply.github.com>
2021-04-24 17:51:56 +0000

    deploy: B3T4C0R3/webinarspot@378e4a626066f4411097ca1796170cc2c03f01ca

---
+++ text/brand.js
@@ -0,0 +1,16 @@
+const brand = {
+  webinarspot: {
+    name: 'Webinarspot',
+    desc: 'De beste webinars, e-learnings en digitale events!',
+    prefix: 'Webinarspot',
+    footerText: 'Webinarspot 2021',
+    logoText: 'webinarspot',
+    projectName: 'webinarspot',
+    url: 'Webinarspot.nl',
+    img: '/static/images/logo.png',
+    notifMsg:
+      'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
+  }
+}
+
+export default brand
---
+++ text/link.js
@@ -0,0 +1,10 @@
+const link = {
+  webinarspot: {
+    home: '/',
+    contact: '/contact',
+    login: '/login',
+    register: '/register'
+  }
+}
+
+export default link
